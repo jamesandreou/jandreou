7b771640f77e191f843b00eb7e219af1183413dc
James Andreou <jandreou25@gmail.com>
2016-03-26 22:50:21 -0400

    fixed server

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ var config = require('./webpack.config.dev');
 var bodyParser = require("body-parser");
 var favicon = require('serve-favicon');
 var fs = require('fs');
-var basicAuth = require('basic-auth-connect');
-var info = require(path.join(__dirname, 'info.json'));
 
 var app = express();
-app.use(bodyParser.json());      
-app.use(bodyParser.urlencoded({    
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
@@ -23,22 +21,12 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 app.use(require('webpack-hot-middleware')(compiler));
 
-//auth
-
-var auth = basicAuth(function(username,password,callback){
-  var result = (password === info.password);
-  callback(null, result);
-});
-
 //routing
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/jobs', auth, function(req, res){
-  res.sendFile(path.join(__dirname, '/pages/jobs.html'));
-});
-
+/*
 app.get('/resume', function(request, response){
   var pdfPath = path.join(__dirname, 'src/assets/resume.pdf');
   fs.readFile(pdfPath, function (err,data){
@@ -46,6 +34,7 @@ app.get('/resume', function(request, response){
      response.send(data);
   });
 });
+*/
 
 app.listen(app.get('port'), function(err) {
   if (err) {
@@ -55,4 +44,3 @@ app.listen(app.get('port'), function(err) {
 
   console.log('Starting jandreou.com');
 });
-
