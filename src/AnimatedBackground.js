15a21aa5ed6e351f36fd1323e548ed160116f5f3
James Andreou <jandreou25@gmail.com>
2016-12-03 18:53:02 -0800

    cleaned up AnimatedBackground and added transforms to radius and color

--- src/AnimatedBackground.js
+++ src/AnimatedBackground.js
@@ -4,40 +4,29 @@ export class AnimatedBackground extends Component {
 
   constructor(){
     super();
+    // Constants
+    this.colors = ['#68D0ED', '#D3F4E3', '#FF7452']
+    this.numEntities = 20
+    this.height = 480
+    // Entities to render
     this.state = {
       entities : this.initalEntities()
     }
-    setInterval((() => {
-      // New entity locations.
-      let entities = [];
-      for (let e of this.state.entities) {
-        // Can this entity move this interval?
-        let newX = e.x, newY = e.y;
-        if (Math.random() > 2 / 3) {
-          // Calculate delta values
-          newX += Math.random() * 30 - 15
-          newY += Math.random() * 30 - 15
-        }
-        // Move entities, if it stays in bounds
-        entities.push(new Entity(
-          (newX < 99 && newX > 1) ? newX : e.x,
-          (newY < 99 && newY > 1) ? newY : e.y
-        ))
-      }
-      this.setState({entities : entities})
-    }).bind(this), 1000)
+    // Incrementally apply trasforms on a set probability every second.
+    setInterval(this.transformEntities.bind(this), 1000)
   }
 
   render() {
     const w = window.innerWidth
-    const h = 480
-    const sizeFactor = h / 100
-    const xFactor = w / 100
-    const yFactor = h / 100
+    const xFactor = window.innerWidth / 100
+    const yFactor = this.height / 100
+    const rFactor = Math.min(xFactor, yFactor)
     return(
       <svg className='entity-container' viewBox={'0 0 ' + w.toString() + ' ' + w.toString()} preserveAspectRatio='xMidYMid meet'>
+        <rect x="0" y="0" width={w} height={this.height} style={{fill : '#fff'}}/>
         {this.state.entities.map((e, i) => (
-          <ellipse key={i} cx={e.x * xFactor} cy={e.y * yFactor} rx={2 * sizeFactor} ry={2 * sizeFactor} className="entity-vertice"/>
+          <circle key={i} cx={e.x * xFactor} cy={e.y * yFactor} r={e.r * rFactor}
+                  className="entity-vertice" style={{fill : this.colors[e.c]}}/>
         ))}
       </svg>
     );
@@ -45,20 +34,55 @@ export class AnimatedBackground extends Component {
 
   initalEntities() {
     let entities = [];
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < this.numEntities; i++) {
       entities.push(new Entity(
         Math.random() * 100,
-        Math.random() * 100
+        Math.random() * 100,
+        Math.random() * 2 + 1,
+        i % 3
       ))
     }
     return entities
   }
 
+  transformEntities() {
+    // Updated entities.
+    let entities = [];
+    for (let e of this.state.entities) {
+      // Probability distribution of transforms.
+      const canMove = Math.random() > 2 / 3
+      const canGrow = Math.random() > 2 / 3
+      const canChangeColor = Math.random() > 2 /3
+
+      // Apply transforms if possible.
+      let newX = e.x, newY = e.y, newR = e.r, newC = e.c;
+      if (canMove) {
+        // Calculate delta values.
+        let dx = Math.random() * 30 - 15
+        let dy = Math.random() * 30 - 15
+        // Only add deltas if bounds are still satisfied.
+        newX += (e.x + dx < 99 && e.x + dx > 1) ? dx : 0
+        newY += (e.y + dy < 99 && e.y + dy > 1) ? dy : 0
+      }
+      if (canGrow) {
+        newR = Math.random() * 2 + 1
+      }
+      if (canChangeColor) {
+        newC = Math.floor(Math.random() * 3)
+      }
+
+      entities.push(new Entity(newX, newY, newR, newC))
+    }
+    this.setState({entities : entities})
+  }
+
 }
 
 class Entity {
-  constructor(x, y){
+  constructor(x, y, r, c){
     this.x = x
     this.y = y
+    this.r = r
+    this.c = c
   }
 }
