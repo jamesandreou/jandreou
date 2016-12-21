72ec11b0ca6085d7f41cda9feca9e41f446f528b
James Andreou <jandreou25@gmail.com>
2016-12-20 20:12:25 -0800

    education cards

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { NavTabs } from './NavTabs.js'
 import { BioCard } from './BioCard.js'
 import { Divider } from './Divider.js'
 import { ExperienceCard } from './ExperienceCard.js'
+import { EducationCard } from './EducationCard.js'
 import * as CardData from './CardData'
 import { Grid, Row } from 'react-flexbox-grid/lib/index'
 import { StickyContainer, Sticky } from 'react-sticky';
@@ -21,11 +22,19 @@ const app = (
     <Grid>
       <BioCard />
       <Divider label='Experience'/>
+      <h2 className='label'>Internships</h2>
       <Row>
         <ExperienceCard data={CardData.experienceCards[0]} />
         <ExperienceCard data={CardData.experienceCards[1]} />
       </Row>
       <Divider label='Education'/>
+      <h2 className='label'>Unviersity of Waterloo</h2>
+      <h3 className='label col2'>Computer Science, Business Option, Combinatorics and Optimization Minor</h3>
+      <Row>
+        {CardData.educationCards.reverse().map((card, i) => (
+          <EducationCard key={i} data={card} />
+        ))}
+      </Row>
       <Divider label='Projects'/>
     </Grid>
   </StickyContainer>
